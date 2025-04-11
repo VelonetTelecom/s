c5418f685441f77e4e2b91463717e786ae4a0ae4
VelonetTelecom <atendimento.velonet@gmail.com>
2025-04-11 15:57:14 -0300

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-
-import dynamic from 'next/dynamic';
-
-const RoletaPontualidade = dynamic(() => import('../RoletaPontualidade'), { ssr: false });
-
-export default function Home() {
-  return <RoletaPontualidade />;
-}
